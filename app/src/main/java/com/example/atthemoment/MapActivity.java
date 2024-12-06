bf66c4c995bb0783619c81fc9013650f4581ac77
RiccardoSilvestri <r.silvestri04@gmail.com>
2024-12-06 12:38:05 +0100

    implemented first map prototype

--- app/src/main/java/com/example/atthemoment/MapActivity.java
+++ app/src/main/java/com/example/atthemoment/MapActivity.java
@@ -3,9 +3,12 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 
-
-public class MapActivity extends  android.app.Activity {
+public class MapActivity extends android.app.Activity {
     private MapView mapView = null;
 
     @Override
@@ -13,9 +16,25 @@ public class MapActivity extends  android.app.Activity {
         super.onCreate(savedInstance);
         Configuration.getInstance().load(getApplicationContext(),
                 PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
-        //TODO check permissions
         setContentView(R.layout.map_activity);
         mapView = findViewById(R.id.mapView);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setBuiltInZoomControls(true);
+        mapView.setMultiTouchControls(true);
+
+        // Imposta un punto centrale
+        mapView.getController().setZoom(22);
+        mapView.getController().setCenter(new GeoPoint(45.483294580054626,9.237441130916924));
+
+        GeoPoint startPoint = new GeoPoint(45.483294580054626, 9.237441130916924);
+        Marker startMarker = new Marker(mapView);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        mapView.getOverlays().add(startMarker);
+
+        MyLocationNewOverlay locationOverlay = new MyLocationNewOverlay(mapView);
+        locationOverlay.enableMyLocation();
+        mapView.getOverlays().add(locationOverlay);
     }
 
     @Override
