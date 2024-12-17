b3193b19813eca04cac0e4be5e58f79e175548be
LeandroBenaglio <157703205+LeandroBenaglio@users.noreply.github.com>
2024-12-17 12:58:10 +0100

    Make LatLon

--- app/src/main/java/it/rizzoli/atthemoment/activity/FindAroundMeActivity.java
+++ app/src/main/java/it/rizzoli/atthemoment/activity/FindAroundMeActivity.java
@@ -1,15 +1,45 @@
 package it.rizzoli.atthemoment.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.atthemoment.R;
 
+import java.io.IOException;
+
 public class FindAroundMeActivity extends AppCompatActivity {
+
+    private TextView test;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_around);
+
+        test = findViewById(R.id.TEST);
+
+        new Thread(() -> {
+            try {
+                double[] coordinates = stampaXY();
+                runOnUiThread(() -> {
+                    test.setText("Latitude: " + coordinates[0] + ", Longitude: " + coordinates[1]);
+                });
+            } catch (IOException e) {
+                Log.e("FindAroundMeActivity", "Errore durante la fetch delle coordinate", e);
+            }
+        }).start();
+    }
+
+    public static double[] stampaXY() throws IOException {
+        Log.e("FindMe", "Fetch iniziato");
+
+        double latitude = 40.7128;
+        double longitude = -74.0060;
+
+        return new double[]{latitude, longitude};
     }
 
     @Override
@@ -27,4 +57,3 @@ public class FindAroundMeActivity extends AppCompatActivity {
         super.onDestroy();
     }
 }
-
